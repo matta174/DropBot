eecb55705549f7e60c1411b14eb89d06c211122e
Matthew Compton <matthewc174@gmail.com>
2021-05-20 16:29:57 -0400

    Initial commit

---
+++ CODBot/Modules/CODModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace CODBot.Modules
+{
+    [Name("COD")]
+    public class CODModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say"), Alias("s")]
+        [Summary("Make the bot say something")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Say([Remainder]string text)
+        { 
+            
+
+            await ReplyAsync(text);
+        }
+    }
+}
---
+++ CODBot/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace CODBot
+{
+    class Program
+    {
+        public static Task Main(string[] args)
+            => Startup.RunAsync(args);
+    }
+}
